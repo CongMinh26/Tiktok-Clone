75c44ddf1eed720da2bb7acc607117028fc71a32
CongMinh26 <congminhad1999@gmail.com>
2022-06-13 22:23:52 +0700

    install babel plugin module resolver

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tiktok-clone
 Build actual products (clone Tiktok)
+
+Install library
+npm i customize-cra react-app-rewired --save-dev
+=> add new file config-overrides.js
+
+npm install --save-dev babel-plugin-module-resolver 
+=> add new file .babelrc
+=> add new jsconfig.json 
--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,11 @@
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
-}
+const {
+  override,
+  useBabelRc
+} = require("customize-cra");
+
+module.exports = override(
+  // override sẽ trả về cấu hình nguyên bản của webpack và exports ra bên ngoài
+  // useBabelRc sẽ ghi đè lên webpack
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useBabelRc()
+);
